092b518e51cef0a1691312f3a60e2a8b421e9248
misaelbarreto <misaelbarreto@gmail.com>
2021-04-30 00:13:05 -0300

    * Configuração para montagem de cluster kubernets.

---
+++ src/build.sh
@@ -0,0 +1,2 @@
+# Execute este script estando dentro da pasta api-produto
+docker image build --no-cache -t misaelbarreto/api-bootcamp-produto:v2 -f Dockerfile .  
---
+++ src/registry.sh
@@ -0,0 +1,4 @@
+# https://hub.docker.com/repository/docker/misaelbarreto/api-bootcamp-produto
+docker push misaelbarreto/api-bootcamp-produto:v2
+docker tag misaelbarreto/api-bootcamp-produto:v2  misaelbarreto/api-bootcamp-produto:latest
+docker push misaelbarreto/api-bootcamp-produto:latest
